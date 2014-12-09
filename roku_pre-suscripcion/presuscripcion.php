ae6df94e876155cfd9b88106d747b52a435e1472
Luis Benítez <contacto@lbenitez.net>
2014-12-09 17:40:37 -0300

    Hoja pre suscriptora en GET

---
+++ roku_pre-suscripcion/presuscripcion.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Roku presuscripción</title>
+	<meta charset="utf-8"> 
+</head>
+<body>
+	<a href="presuscripcion_action.php?titulo=titulo&codigo_canal=ASD123&codigo_dispositivo=asdfg6457689">Pre suscribir</a>
+	
+</body>
+</html>
