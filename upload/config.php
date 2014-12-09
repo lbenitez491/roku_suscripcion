b1e1513f3b4f2b8720ef76d7d45ccbb8873043bd
Luis Benítez <contacto@lbenitez.net>
2014-12-08 23:44:55 -0300

    OpenCart Instalado
    OpenCart Instalado con bd MySQL.

---
+++ upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/~Ludwig/Git/roku/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/~Ludwig/Git/roku/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ludwig/Sites/Git/roku/upload/catalog/');
+define('DIR_SYSTEM', '/Users/Ludwig/Sites/Git/roku/upload/system/');
+define('DIR_LANGUAGE', '/Users/Ludwig/Sites/Git/roku/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Ludwig/Sites/Git/roku/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Ludwig/Sites/Git/roku/upload/system/config/');
+define('DIR_IMAGE', '/Users/Ludwig/Sites/Git/roku/upload/image/');
+define('DIR_CACHE', '/Users/Ludwig/Sites/Git/roku/upload/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ludwig/Sites/Git/roku/upload/system/download/');
+define('DIR_UPLOAD', '/Users/Ludwig/Sites/Git/roku/upload/system/upload/');
+define('DIR_MODIFICATION', '/Users/Ludwig/Sites/Git/roku/upload/system/modification/');
+define('DIR_LOGS', '/Users/Ludwig/Sites/Git/roku/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Awdsawds2');
+define('DB_DATABASE', 'roku_opencart');
+define('DB_PREFIX', '');
